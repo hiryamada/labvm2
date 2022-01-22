b9a9029412480874c9c07c01c63cd118134af39b
hiryamada <71864722+hiryamada@users.noreply.github.com>
2022-01-22 17:31:00 +0900

    Update labvm.ps1

--- labvm.ps1
+++ labvm.ps1
@@ -1,3 +1,12 @@
+# Set Processor Scheduling to "Programs" (default: "Background services")
+echo 'Windows Registry Editor Version 5.00
+
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\PriorityControl]
+"Win32PrioritySeparation"=dword:00000026
+
+' > PriorityControl.reg
+regedit /s PriorityControl.reg
+
 # install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
