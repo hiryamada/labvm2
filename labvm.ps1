7b7406008b1c6009ee7481f7b94a019f2cc0baf0
hiryamada <71864722+hiryamada@users.noreply.github.com>
2022-02-04 21:03:34 +0900

    Update labvm.ps1

--- labvm.ps1
+++ labvm.ps1
@@ -11,17 +11,20 @@ regedit /s PriorityControl.reg
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # install packages
-choco install -y git azure-cli az.powershell vscode dotnet-6.0-sdk-1xx microsoftazurestorageexplorer googlechrome setdefaultbrowser azure-data-studio microsoft-windows-terminal GoogleJapaneseInput powershell-core sakuraeditor quicklook sharex wox
+choco install -y git azure-cli az.powershell vscode dotnet-6.0-sdk-1xx microsoftazurestorageexplorer googlechrome setdefaultbrowser azure-data-studio microsoft-windows-terminal GoogleJapaneseInput powershell-core sakuraeditor quicklook sharex wox copyq
 
 # install Azure Function Core Tools 4.x (choco has only 3.x)
 iwr -outf func-cli-x64.msi https://go.microsoft.com/fwlink/?linkid=2174087
 msiexec /i "func-cli-x64.msi" /passive /norestart
 
 # install httprepl
-& "C:\Program Files\dotnet\dotnet.exe" tool install -g Microsoft.dotnet-httprepl
+& "C:\Program Files\dotnet\dotnet.exe" tool install -g Microsoft.dotnet-httprepl"
 
 refreshenv
 
 # set Edge as default browser
 # SetDefaultBrowser.exe HKLM "Microsoft Edge" delay=1000
 SetDefaultBrowser.exe chrome
+
+# set Timezone
+tzutil.exe /s "Tokyo Standard Time"
