563fe7d96cca8301ee63a7aefd33eedb5170c84d
hiryamada <71864722+hiryamada@users.noreply.github.com>
2022-03-05 09:15:27 +0900

    Update labvm.ps1

--- labvm.ps1
+++ labvm.ps1
@@ -11,7 +11,8 @@ regedit /s PriorityControl.reg
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # install packages
-choco install -y git azure-cli az.powershell vscode dotnet-6.0-sdk-1xx microsoftazurestorageexplorer googlechrome setdefaultbrowser azure-data-studio microsoft-windows-terminal GoogleJapaneseInput powershell-core sakuraeditor quicklook sharex wox copyq insomnia-rest-api-client postman velocity
+# choco install -y git azure-cli az.powershell vscode dotnet-6.0-sdk-1xx microsoftazurestorageexplorer googlechrome setdefaultbrowser azure-data-studio microsoft-windows-terminal GoogleJapaneseInput powershell-core sakuraeditor quicklook sharex wox copyq insomnia-rest-api-client postman velocity
+cinst -y git azure-cli az.powershell googlechrome vscode dotnet setdefaultbrowser microsoft-windows-terminal cyberduck
 
 # install Azure Function Core Tools 4.x (choco has only 3.x)
 iwr -outf func-cli-x64.msi https://go.microsoft.com/fwlink/?linkid=2174087
